2f9b6d5fc888edb0955bcc66ecaf8b91080693a6
Eddy(최준우) <junwoochoi@zuminternet.com>
2020-06-03 09:48:29 +0900

    test: add test

--- src/test/java/domain/NormalFrameTest.java
+++ src/test/java/domain/NormalFrameTest.java
@@ -1,10 +1,14 @@
 package domain;
 
 import domain.frame.impl.NormalFrame;
+import domain.pin.Pins;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -56,6 +60,44 @@ class NormalFrameTest {
 
         assertThrows(IllegalArgumentException.class, () ->
                 normalFrame.throwBowlingBall(2));
+    }
 
+    @Test
+    @DisplayName("첫 투구가 스트라이크일 때, 해당 턴은 끝난 것으로 인식된다")
+    void testFirstStrikeEndTurn() {
+        final NormalFrame normalFrame = NormalFrame.newInstanceByFrameNumber(1);
+
+        normalFrame.throwBowlingBall(Pins.MAX_NUMBER_OF_PINS);
+
+        final boolean isDone = normalFrame.isDone();
+
+        assertThat(isDone).isTrue();
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 20})
+    @DisplayName("공을 던졌을 때, 핀의 갯수를 초과하거나 음수이면 Exception을 던진다")
+    void testThrowBallInvalidPinCount(int fallenPins) {
+        final NormalFrame normalFrame = NormalFrame.newInstanceByFrameNumber(1);
+
+        assertThrows(IllegalArgumentException.class, () ->
+                normalFrame.throwBowlingBall(fallenPins)
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 6, 10})
+    @DisplayName("공을 던졌을 때, 넘어진 핀의 갯수에 대한 기록을 추가한다")
+    void testThrowBallSuccess(int fallenPins) {
+        final NormalFrame normalFrame = NormalFrame.newInstanceByFrameNumber(1);
+
+        normalFrame.throwBowlingBall(fallenPins);
+
+
+        final List<Integer> frameHistories = normalFrame.getFrameHistories();
+        final Integer lastHistory = frameHistories.get(frameHistories.size() - 1);
+
+        assertThat(lastHistory).isEqualTo(fallenPins);
     }
 }
